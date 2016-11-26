4a6adfc84d0fb89231be2cab8e1b6ddb1166a3b2
Harold Mendoza <hrmendoza96@unitec.edu>
2016-11-26 13:25:58 -0600

    Commit 1

---
+++ Racional.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Racional{
+  private:
+    int numerador;
+    int denominador;
+
+
+  public:
+    Racional();
+
+
+
+    string toString();
+
+
+
+
+
+
+
+
+
+
+};
